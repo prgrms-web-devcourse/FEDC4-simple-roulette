bc98f0a5f85dc60cd550ecb7c447eca7fe8aef56
hyeonjun-L <beeminor60@gmail.com>
2023-06-16 21:09:20 +0900

    Feat & Fix: Roullet setState 기능 &  룰렛 안그려지는 버그 해결

--- src/components/roullet-section/RoulletCanvas.js
+++ src/components/roullet-section/RoulletCanvas.js
@@ -1,6 +1,11 @@
 function RoulletCanvas({ $canvas, initialState, sumRatios }) {
   this.state = initialState;
 
+  this.setState = (nextState) => {
+    this.state = nextState;
+    this.render(this.circleState);
+  };
+
   this.circleState = {
     ctx: $canvas.getContext("2d"),
     centerX: $canvas.width / 2,
@@ -10,7 +15,7 @@ function RoulletCanvas({ $canvas, initialState, sumRatios }) {
 
   this.render = ({ ctx, centerX, centerY, radius }) => {
     // 룰렛 원 영역
-    ctx.beginPath();
+
     ctx.arc(centerX, centerY, radius, 0, 2 * Math.PI); // 원의중심 x좌표, 원의 중심 y좌표, 원의 반지름, 원호의 시작 각도, 원호의 종료 각도
     ctx.strokeStyle = "#DBDBDB"; //룰렛선 색상
 
@@ -25,10 +30,12 @@ function RoulletCanvas({ $canvas, initialState, sumRatios }) {
       const endAngle = radian + startAngle;
 
       ctx.beginPath();
+      ctx.moveTo(centerX, centerY);
       ctx.arc(centerX, centerY, radius, startAngle, endAngle);
       ctx.closePath();
       ctx.fillStyle = key % 2 === 0 ? "white" : "gray"; // 색깔 입히는곳
       ctx.fill();
+      ctx.stroke();
 
       startAngle = endAngle;
     }
--- src/components/roullet-section/roullet-section.js
+++ src/components/roullet-section/roullet-section.js
@@ -9,26 +9,33 @@ function RoulletSection() {
     {
       key: 1,
       value: "쉬운 난이도",
-      checked: true,
-      ratio: 12,
+      checked: false,
+      ratio: 1,
     },
     {
       key: 2,
       value: "중간 난이도",
       checked: false,
-      ratio: 2,
+      ratio: 1,
     },
     {
       key: 3,
       value: "어려운 난이도",
       checked: false,
-      ratio: 2,
+      ratio: 1,
+    },
+    {
+      key: 4,
+      value: "어려운 난이도",
+      checked: false,
+      ratio: 1,
     },
   ];
   //localstorage get으로 수정 예정
 
   const $canvas = document.querySelector("canvas");
-  new RoulletCanvas({
+
+  const Roullet = new RoulletCanvas({
     $canvas,
     initialState: lists,
     sumRatios: (lists) => {
