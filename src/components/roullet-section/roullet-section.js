835bde3b67eb8b038d2a61c1fb8dda8af16aea07
hyeonjun-L <beeminor60@gmail.com>
2023-06-16 22:11:00 +0900

    Test: 인스턴스 export

--- src/components/roullet-section/roullet-section.js
+++ src/components/roullet-section/roullet-section.js
@@ -1,39 +1,43 @@
 import html from "./roullet-section.html";
 import "./roullet-section.css";
-import RoulletCanvas from "./RoulletCanvas";
+import RoulletCanvas from "./roullet-canvas";
+
+const lists = [
+  {
+    key: 1,
+    value: "쉬운 난이도",
+    checked: false,
+    ratio: 1,
+  },
+  {
+    key: 2,
+    value: "중간 난이도",
+    checked: false,
+    ratio: 1,
+  },
+  {
+    key: 3,
+    value: "어려운 난이도",
+    checked: false,
+    ratio: 1,
+  },
+  {
+    key: 4,
+    value: "어려운 난이도",
+    checked: false,
+    ratio: 1,
+  },
+];
+//localstorage get으로 수정 예정
+// const lists = storage.getItem("list",[])
 
 function RoulletSection() {
   const $wrapper = document.querySelector(".roullet-section");
   $wrapper.insertAdjacentHTML("afterbegin", html);
-  const lists = [
-    {
-      key: 1,
-      value: "쉬운 난이도",
-      checked: false,
-      ratio: 1,
-    },
-    {
-      key: 2,
-      value: "중간 난이도",
-      checked: false,
-      ratio: 1,
-    },
-    {
-      key: 3,
-      value: "어려운 난이도",
-      checked: false,
-      ratio: 1,
-    },
-    {
-      key: 4,
-      value: "어려운 난이도",
-      checked: false,
-      ratio: 1,
-    },
-  ];
-  //localstorage get으로 수정 예정
 
   const $canvas = document.querySelector("canvas");
+
+  console.log($canvas);
 
   const Roullet = new RoulletCanvas({
     $canvas,
