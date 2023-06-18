b93fc3c65116b6c0d44ac8e6d74383dd70e1e33f
daeun <gbajs754@gmail.com>
2023-06-18 17:01:50 +0900

    Feat: 룰렛 돌아가는 기능 추가

--- src/components/roullet-section/roullet-random-item.js
+++ src/components/roullet-section/roullet-random-item.js
@@ -4,6 +4,7 @@ function RandomItemBtn({
   sumRatio,
   getResult,
   setResult,
+  $canvas
 }) {
   this.state = initialState;
 
@@ -12,22 +13,39 @@ function RandomItemBtn({
     this.render;
   };
 
+  this.rotate = (totalRatio, randomValue) => {
+    $canvas.style.transform = `initial`;
+    $canvas.style.transition = `initial`;
+    console.log(randomValue) // **추후 콘솔 삭제**
+  
+    setTimeout(()=>{
+      const angle = 360 / totalRatio;
+      const rotate = (randomValue * angle) + 3600 +(angle/2)
+
+      $canvas.style.transform = `rotate(-${rotate}deg)`
+      $canvas.style.transition = `3s`;
+    },0);
+    
+  };
+
   this.render = (() => {
     $button.addEventListener("click", (e) => {
       const totalRatio = sumRatio(this.state);
-      const randomValue = Math.random() * totalRatio;
+      const randomValue = Math.floor(Math.random() * totalRatio);
+      this.rotate(totalRatio, randomValue); 
 
       let accumulatedRatio = 0;
       for (const { ratio, value } of this.state) {
         accumulatedRatio += ratio;
-        if (randomValue <= accumulatedRatio) {
+        if (randomValue < accumulatedRatio) {
           console.log(value); // **추후 콘솔 삭제**
           setResult("results", [...getResult("results", []), value]);
           return value;
         }
       }
     });
-  })();
+  }
+  )();
 }
 
 export default RandomItemBtn;
