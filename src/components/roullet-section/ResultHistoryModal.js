4e8bdeb0d32fd17c423fba2ba11085126b88c61b
Eunseo-jo <90231153+Eunseo-jo@users.noreply.github.com>
2023-06-20 01:23:46 +0900

    feat: add results history modal

---
+++ src/components/roullet-section/ResultHistoryModal.js
@@ -0,0 +1,55 @@
+import { storage } from "../../storage";
+
+export default function ResultHistoryModal() {
+  const openButton = document.getElementById("open-history-modal");
+  const modal = document.querySelector(".history-modal");
+  const overlay = modal.querySelector(".history-modal_overlay");
+  const closeBtn = modal.querySelector("button");
+  const resultList = document.querySelector(".results-section");
+  const clearBtn = modal.querySelector(".clear-btn");
+
+  const renderResults = () => {
+    resultList.innerHTML = "";
+    const results = storage.getItem("results", []);
+
+    if (results.length === 0) {
+      //결과 내역이 없을 때
+      const message = document.createElement("p");
+      message.textContent = "결과 내역이 없습니다";
+      resultList.appendChild(message);
+    } else {
+      //결과 리스트 렌더링
+      results.forEach((item, index) => {
+        const resultItem = document.createElement("div");
+        resultItem.classList = "results-item";
+        resultItem.innerHTML = `
+            <div class="result_index"> ${index + 1} </div>
+            <p> ${item} </p> 
+        `;
+        resultList.appendChild(resultItem);
+      });
+    }
+  };
+
+  const clearHistory = () => {
+    //결과 전체 삭제
+    storage.clearItem("results");
+    renderResults();
+  };
+
+  const openModal = () => {
+    //모달 열기
+    modal.classList.remove("hidden");
+    renderResults();
+  };
+
+  const closeModal = () => {
+    //모달 닫기
+    modal.classList.add("hidden");
+  };
+
+  overlay.addEventListener("click", closeModal);
+  openButton.addEventListener("click", openModal);
+  closeBtn.addEventListener("click", closeModal);
+  clearBtn.addEventListener("click", clearHistory);
+}
