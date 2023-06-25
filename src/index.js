3f8fcefc1ff1ddea77a40471f41a76f8137d92de
Eunseo-jo <90231153+Eunseo-jo@users.noreply.github.com>
2023-06-25 20:22:04 +0900

    refactor: rm initialState

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import App from './components/App.js';
-import './index.css';
+import App from "./components/App.js";
+import "./index.css";
 
-const $target = document.getElementById('app');
-new App({ $target, initialState: '기본 상태 값으로 무엇을 보낼까요오' });
+const $target = document.getElementById("app");
+new App({ $target });
