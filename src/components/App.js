556929c86b2fafa9835156169a6d5b0cfdcf1bfb
Eunseo-jo <90231153+Eunseo-jo@users.noreply.github.com>
2023-06-25 20:22:32 +0900

    feat: add rolletHistorySection Component

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,37 @@
-import Header from './header/Header.js';
-import ItemSection from './itemSection/ItemSection.js';
-import RoulletSection from './roulletSection/RoulletSection.js';
-import RoulletHistorySection from './roulletHistorySection/RoulletHistorySection.js';
+import Header from "./header/Header.js";
+import ItemSection from "./itemSection/ItemSection.js";
+import RoulletSection from "./roulletSection/RoulletSection.js";
+import RoulletHistorySection from "./roulletHistorySection/RoulletHistorySection.js";
+import ResultsStore from "../stores/resultStore.js";
 
 export default class App {
-  constructor({ $target, initialState }) {
-    this.state = initialState;
+  constructor({ $target }) {
+    this.$target = $target;
+    this.resultsStore = new ResultsStore(); //결과 내역을 관리하는 스토어
 
-    new Header({ $target: document.querySelector('#header') });
-    new ItemSection({ $target: document.querySelector('.item-section') });
-    new RoulletSection({ $target: document.querySelector('.roullet-section') });
-    new RoulletHistorySection({ $target: document.querySelector('.roullet-history-section') });
-
+    this.initialComponents();
     this.render();
   }
 
-  setState(nextState) {
-    this.state = nextState;
-    this.render();
+  initialComponents() {
+    new Header({ $target: this.$target.querySelector("#header") });
+    new ItemSection({ $target: this.$target.querySelector(".item-section") });
+    new RoulletSection({
+      $target: this.$target.querySelector(".roullet-section"),
+    });
+
+    this.roulletHistorySection = new RoulletHistorySection({
+      $target: this.$target.querySelector(".roullet-history-section"),
+      initialState: this.resultsStore.state.results,
+      clearHistory: () => {
+        this.resultsStore.deleteAllResults();
+        this.render();
+      },
+    });
   }
 
-  render() {}
+  render() {
+    const resultState = this.resultsStore.state.results; //스토어에서 state가져오기
+    this.roulletHistorySection.setState(resultState); //state 재설정
+  }
 }
