826923b06a1d511413eeb465b89479dd042341b6
bbearcookie <50488780+bbearcookie@users.noreply.github.com>
2023-07-08 14:58:06 +0900

    refactor: storage를 도메인별로 추상화 했던 부분 해제

--- src/stores/itemListStore.js
+++ src/stores/itemListStore.js
@@ -1,5 +1,5 @@
-import ItemListStorage from '../utils/itemListStorage.js';
-import ItemIdStorage from '../utils/itemIdStorage.js';
+import { storage } from '../utils/storage.js';
+import { ITEM_LISTS, ITEM_ID } from '../constants/storageKeys.js';
 
 export const defaultItemList = [
   {
@@ -14,34 +14,32 @@ export const defaultItemId = 1;
 
 class ItemListStore {
   constructor() {
-    this.itemListStorage = new ItemListStorage();
-    this.itemIdStorage = new ItemIdStorage();
-    this.state = this.itemListStorage.getLists();
+    this.state = storage.getItem(ITEM_LISTS, defaultItemList);
   }
 
   setState(nextState) {
     this.state = nextState;
-    this.itemListStorage.setLists(nextState);
+    storage.setItem(ITEM_LISTS, JSON.stringify(nextState));
   }
 
   refreshList() {
     const confirmation = confirm('정말로 모든 항목을 초기화 하시겠습니까?');
     if (!confirmation) return;
-    
+
     this.state = defaultItemList;
-    this.itemListStorage.setLists(defaultItemList);
-    this.itemIdStorage.setItemId(defaultItemId);
+    storage.setItem(ITEM_LISTS, JSON.stringify(defaultItemList));
+    storage.setItem(ITEM_ID, JSON.stringify(defaultItemId));
   }
 
   addItem() {
-    const { itemIdStorage, state } = this;
+    const { state } = this;
 
     if (state.length >= 10) {
       alert('항목은 최대 10개 까지 추가할 수 있어요!');
       return;
     }
 
-    const nextId = itemIdStorage.getItemId();
+    const nextId = storage.getItem(ITEM_ID, defaultItemId);
 
     const newItem = {
       key: nextId,
@@ -50,29 +48,29 @@ class ItemListStore {
       ratio: 1,
     };
 
-    itemIdStorage.setItemId(nextId + 1);
+    storage.setItem(ITEM_ID, JSON.stringify(nextId + 1));
 
     const nextState = [...state, newItem];
     this.setState(nextState);
   }
 
   removeItem(key) {
-    const nextState = this.state.filter(item => item.key !== key);
+    const nextState = this.state.filter((item) => item.key !== key);
     this.setState(nextState);
   }
 
   setCheck(key, checked) {
-    const nextState = this.state.map(item => (item.key === key ? { ...item, checked } : item));
+    const nextState = this.state.map((item) => (item.key === key ? { ...item, checked } : item));
     this.setState(nextState);
   }
 
   setValue(key, value) {
-    const nextState = this.state.map(item => (item.key === key ? { ...item, value } : item));
+    const nextState = this.state.map((item) => (item.key === key ? { ...item, value } : item));
     this.setState(nextState);
   }
 
   setRatio(key, ratio) {
-    const nextState = this.state.map(item => (item.key === key ? { ...item, ratio } : item));
+    const nextState = this.state.map((item) => (item.key === key ? { ...item, ratio } : item));
     this.setState(nextState);
   }
 }
