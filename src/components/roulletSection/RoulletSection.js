e5b427dfa01076106835aa4c94a06633e36cded3
hyeonjun-L <beeminor60@gmail.com>
2023-07-14 16:00:37 +0900

    Fix: RoulletSection, canvas class 변경

--- src/components/roulletSection/RoulletSection.js
+++ src/components/roulletSection/RoulletSection.js
@@ -1,12 +1,47 @@
 export default class RoulletSection {
   constructor({ $target, initialState }) {
+    this.$wrapper = document.querySelector(".roullet-section");
+    $wrapper.insertAdjacentHTML("afterbegin", html);
+    this.$button = document.querySelector(".start-button");
+    this.$canvas = document.querySelector(".roullet-roll");
+    this.lists = storage.getItem("item_lists", []);
+
     this.$target = $target;
 
     this.state = initialState;
-    this.render();
+    this.initComponent();
   }
 
-  render() {
-    this.$target.innerHTML = '룰렛을 렌더링 할 위치';
+  initComponent() {
+    this.Roullet = new RoulletCanvas({
+      $canvas,
+      initialState: lists,
+      sumRatio: (lists) =>
+        lists.reduce(
+          (acc, { ratio, checked }) => (!checked ? acc : acc + ratio),
+          0
+        ),
+    });
+
+    this.StartBtn = new RandomItemBtn({
+      $button,
+      initialState: lists,
+      sumRatio: (lists) =>
+        lists.reduce(
+          (acc, { ratio, checked }) => (!checked ? acc : acc + ratio),
+          0
+        ),
+      setResult: storage.setItem.bind(storage),
+      getResult: storage.getItem.bind(storage),
+      $canvas,
+    });
+  }
+
+  setState(nextState) {
+    this.state = nextState;
+
+    //수정 필**
+    this.StartBtn.setState(nextState);
+    this.Roullet.setState(nextState);
   }
 }
