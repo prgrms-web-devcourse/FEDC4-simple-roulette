c268459d3533ff6601e04f56e28a8435fdd96fd6
kimko <gbajs754@gmail.com>
2023-07-14 17:29:39 +0900

    Refactor: 세션 수정

--- src/constants/storageKeys.js
+++ src/constants/storageKeys.js
@@ -1,16 +1,3 @@
 export const ITEM_LISTS = "item_lists";
 export const ITEM_ID = "item_id";
 export const RESULTS = "results";
-
-export const colors = [
-  "#E4EAFF",
-  "#C2D0FF",
-  "#A1B5FE",
-  "#7F9BFE",
-  "#5D81FE",
-  "#3C66FD",
-  "#1A4CFD",
-  "#0237F3",
-  "#022FD1",
-  "#0128B0",
-];
