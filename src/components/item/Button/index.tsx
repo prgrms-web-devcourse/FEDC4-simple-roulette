36cf094c5f81438f0ea58fb2a37517d867912ae2
cloud0406 <cloud0406@naver.com>
2023-09-04 00:33:07 +0900

    feat: 공통 버튼 컴포넌트 구현
    * 각 영역들에서 사용되는 공통 버튼 컴포넌트화

---
+++ src/components/item/Button/index.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+interface ButtonProps {
+  label: string;
+  color: 'white' | 'red' | 'blue' | 'gray';
+  iconName: string;
+  onClick: () => void;
+}
+
+const Button2 = ({ label = '', color = 'white', iconName = '', ...props }: Partial<ButtonProps>) => {
+  return (
+    <StyledButton
+      color={color}
+      {...props}>
+      {iconName && (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 448 512"
+          width="12">
+          <path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z"></path>
+        </svg>
+      )}
+      {label}
+    </StyledButton>
+  );
+};
+
+const StyledButton = styled.button<{
+  color: 'white' | 'red' | 'blue' | 'gray';
+}>`
+  color: ${({ color }) => (color === 'white' ? 'black' : 'white')};
+  fill: ${({ color }) => (color === 'white' ? 'black' : 'white')};
+  background-color: ${({ color }) => {
+    if (color === 'white') return 'white';
+    else if (color === 'blue') return '#4e74ff';
+    else if (color === 'red') return '#ff4863';
+    else if (color === 'gray') return 'gray';
+  }};
+  /* border: ${({ color }) => (color === 'white' ? '2px solid lightgray' : 'none')}; */
+  border: none;
+  box-shadow: ${({ color }) => color === 'white' && '0px 4px 16px rgba(0, 0, 0, 0.15)'};
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  padding: 15px;
+  font-size: 16px;
+  border-radius: 10px;
+  text-align: center;
+
+  cursor: pointer;
+`;
+
+export default Button2;
